4491900590a97860b10676fb009e8090430b0f86
R1dhaa <fatmaridha999@gmail.com>
2025-02-08 19:10:47 +0530

    App.js

--- alarm/src/App.js
+++ alarm/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState,useEffect} from  "react";
+const AlarmClock = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  const [snoozeCount, setSnoozeCount] = useState(0)
+  const [alarmTime, setAlarmTime] = useState("");
+  const [isAlarmOn, setAlarmOn] = useState(false);
 }
 
-export default App;
+
+
