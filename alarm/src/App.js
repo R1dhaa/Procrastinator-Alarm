bcacd0d09807666b767f61e320e69f974b14d24a
GayathriS18 <161859459+GayathriS18@users.noreply.github.com>
2025-02-08 20:01:26 +0530

    App.js

--- alarm/src/App.js
+++ alarm/src/App.js
@@ -7,12 +7,25 @@ const AlarmClock = () => {
 }
 useEffect(() => {
   const interval = setInterval(() => {
-    setTime(new Date().toLocaleTimeString());
-    if (isAlarmOn && alarmTime === new Date().toLocaleTimeString()) {
+    const now =new Date();
+    const formattedTime=now.getHours()+":"+now.getMinutes();
+    if (isAlarmOn && alarmTime === formattedTime) {
       alert("Wake up! Time to stop procrastinating!");
     }
   }, 1000);
   return () => clearInterval(interval);
 }, [alarmTime, isAlarmOn]);
-
-
+const handleSnooze = () => {
+  setSnoozeCount(snoozeCount + 1);
+  if (snoozeCount < 2) {
+    alert("Snoozed! Next time it won't be this easy...");
+  } else {
+    let challenge = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10);
+    let userAnswer = prompt(Solve this to snooze: ${challenge} + ${challenge} = ?);
+    if (parseInt(userAnswer) === challenge * 2) {
+      alert("Fine, you can snooze.");
+    } else {
+      alert("Wrong answer! Alarm stays on!");
+    }
+  }
+};
